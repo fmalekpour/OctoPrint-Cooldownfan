1cef578f580debf90fd263d1b65159d3fe1dc301
fmalekpour <fm@farhad.ca>
2020-07-16 18:16:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Turn on the cooldown fan after prining finished"""
+plugin_description = """Turn on the cooldown fan after printing finished"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Farhad Malekpour"
